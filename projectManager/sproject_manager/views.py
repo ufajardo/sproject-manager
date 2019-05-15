e0630cc596629f5652e5b7427c19f36159c16d7b
ufajardo <ulysses.fajardo214@gmail.com>
2019-05-14 23:26:36 -0700

    Added tables and trying out delete functions

--- projectManager/sproject_manager/views.py
+++ projectManager/sproject_manager/views.py
@@ -1,6 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Project, Task
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'sproject_manager/index.html')
+    project_list = Project.objects.all()
+    task_list = Task.objects.all()
+
+
+    context = {
+        'project_list': project_list,
+        'task_list': task_list,
+    }
+    return render(request, 'sproject_manager/index.html', context)
+
+
+def task_item(request, id):
+    task = get_object_or_404(Task, id=id)
+
+    if request.method == "POST":
+        if 'delete' in request.POST:
+            task.delete()
+            return render(request, 'sproject_manager/index.html')
+
+    context = {
+        'task': task,
+    }
+
+    return render(request, 'sproject_manager/task-details.html', context)
